a2bae09986370c2382c11fb93467599b1a1d0629
Anish Deshpande <anish.deshpande@lntinfotech.com>
2021-03-26 09:57:49 +0530

    add postgres for deployment

--- server.py
+++ server.py
@@ -8,8 +8,13 @@ from routes.matchRoute import matchBlueprint
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.secret_key = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.getcwd()}/serverdb.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.getcwd()}/serverdb.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
 
 CORS(app)
 
